4347cd8170900252a1ce67eed88019ffa1c68ae4
Erico Borgonove <borgonove@gmail.com>
2022-08-20 11:50:08 -0400

    comint das primeiras coorecoes

--- app/Services/UploadService.php
+++ app/Services/UploadService.php
@@ -1,10 +1,11 @@
 <?php
 namespace app\Services;
 
-class UploadService{
-    public static function upload($arquivo){
+class UploadService 
+{
+    public static function upload($arquivo)
+    {
         $arquivo->storeAs('public', $arquivo->getClientOriginalName());
         return '/storage/' . $arquivo->getClientOriginalName();
     }
 
-}
